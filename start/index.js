e67fe89929f17ba6d5b23f13e436e108736b9de9
AbdulAhadera <abdulahad.era@gmail.com>
2023-12-13 22:52:35 -0600

    daily task

---
+++ start/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/ahad', (req, res) => {
+    res.send('Hello Ahad!')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
